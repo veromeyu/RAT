b00b163ca10bae517d70558a8954ab34a261f51e
veromeyu <veronicameyu@gmail.com>
2015-03-13 13:03:12 +0100

    aaa

--- app/src/main/java/seginf/com/RatAnd/opcion/camara.java
+++ app/src/main/java/seginf/com/RatAnd/opcion/camara.java
@@ -1,33 +1,171 @@
 package seginf.com.RatAnd.opcion;
 
 
-import android.app.Fragment;
-import android.hardware.camera2.CameraDevice;
-import android.view.View;
+import android.annotation.TargetApi;
+import android.app.*;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.*;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.*;
+import android.media.*;
+
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+
+import seginf.com.R;
+
+import static android.view.TextureView.*;
 
 /**
  * Created by Vero on 26/02/2015.
  */
-public class camara extends Fragment implements  View.OnClickListener {
-    public void onOpened(CameraDevice cameraDevice) {
-
-  // This method is called when the camera is opened.  We start camera preview here.
-
-        mCameraOpenCloseLock.release();
-
-        mCameraDevice = cameraDevice;
-
-        createCameraPreviewSession();
-
-    }
-
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class Camara extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.image: {
+                takePicture();
+                break;
+            }
+        }
 
     }
 
-}
+    private static final String TAG = "Camara";
+    private String mCameraId;
+    private CameraCaptureSession mCaptureSession;
+    private CameraDevice mCameraDevice;
+    private Handler mBackgroundHandler;
+    /**
+     * A {@link Semaphore} to prevent the app from exiting before closing the camera.
+     */
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
+    private HandlerThread mBackgroundThread;
+    private ImageReader mImageReader;
+    private CaptureRequest mPreviewRequest;
+    private CaptureRequest.Builder mPreviewRequestBuilder;
+    private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            mCameraOpenCloseLock.release();
+            camera.close();
+            mCameraDevice = null;
 
+        }
 
-}
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            mCameraOpenCloseLock.release();
+            camera.close();
+            mCameraDevice = null;
+            Activity activity = getActivity();
+            if (null != activity) {
+                activity.finish();
+            }
 
+        }
+
+        @Override
+        public void onOpened(CameraDevice cameraDevice) {
+
+            // This method is called when the camera is opened.  We start camera preview here.
+
+            mCameraOpenCloseLock.release();
+
+            mCameraDevice = cameraDevice;
+
+            createCameraPreviewSession();
+
+        }
+    };
+    private void openCamera(int width, int height) {
+        Activity activity = getActivity();
+        CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (!mCameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
+                throw new RuntimeException("Time out waiting to lock camera opening.");
+            }
+            manager.openCamera(mCameraId, mStateCallback, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera opening.", e);
+        }
+    }
+    private void closeCamera() {
+        try {
+            mCameraOpenCloseLock.acquire();
+            if (null != mCaptureSession) {
+                mCaptureSession.close();
+                mCaptureSession = null;
+            }
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                mCameraDevice = null;
+            }
+            if (null != mImageReader) {
+                mImageReader.close();
+                mImageReader = null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera closing.", e);
+        } finally {
+            mCameraOpenCloseLock.release();
+        }
+    }
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private void createCameraPreviewSession(){
+            try {
+                mPreviewRequestBuilder= mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                mCameraDevice.createCaptureSession(Arrays.asList(mImageReader.getSurface()),
+                        new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(CameraCaptureSession session) {
+                                // The camera is already closed
+                                if (null == mCameraDevice) {
+                                    return;
+                                }
+            // When the session is ready, we start displaying the preview.
+                                mCaptureSession = session;
+                                try {
+                // we start displaying the camera preview.
+                                    mPreviewRequest = mPreviewRequestBuilder.build();
+                                    mCaptureSession.setRepeatingRequest(mPreviewRequest,
+                                            mCaptureCallback, mBackgroundHandler);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                            @Override
+                            public void onConfigureFailed(CameraCaptureSession session) {
+
+                            }
+                        });
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+    }
+ }
